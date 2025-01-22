62ffa23dc0aae7d1bad580126f19632c0e079a97
J-millar99 <wlwogus33@gamil.com>
2025-01-22 21:44:18 +0900

    Location Parser를 구분하여 소스코드 추가

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 NAME = webserv
 CPP = c++
-CXXFLAGS = -Wall -Wextra -Werror -std=c++98
+# CXXFLAGS = -Wall -Wextra -Werror -std=c++98
 
 SRCS = sources/main.cpp sources/utils.cpp \
 		sources/system/System.cpp \
@@ -8,13 +8,13 @@ SRCS = sources/main.cpp sources/utils.cpp \
 		sources/system/SystemUtils.cpp \
 		sources/server/Server.cpp \
 		sources/server/ServerParser.cpp \
+		sources/server/LocationParser.cpp \
 		sources/server/ServerUtils.cpp \
 		sources/server/Receive.cpp \
 		sources/server/Response.cpp \
 		sources/http/HttpHeader.cpp \
 		sources/http/HttpRequest.cpp \
 		sources/http/HttpResponse.cpp \
-		
 		
 OBJS = $(SRCS:.cpp=.o)
 
--- sources/server/LocationParser.cpp
+++ sources/server/LocationParser.cpp
@@ -1,6 +1,7 @@
 #include "Server.hpp"
 
 void Server::parseLocationBlock(std::string &locationBlock) {
+    return_flag = false;
     std::string::size_type idx = 0;
     std::string::size_type start = locationBlock.find_first_of('{');
     std::string LocationDirective = locationBlock.substr(0, start);
@@ -8,7 +9,6 @@ void Server::parseLocationBlock(std::string &locationBlock) {
     std::vector<std::string> locationString = splitString(LocationDirective);
     locationBlock = locationBlock.substr(start + 1, locationBlock.size() - start);
     Location loc;
-    loc.methods = 0;
 
     if (locationString[0] != "location")
         throw std::runtime_error("Missing 'Location' directive");
@@ -23,7 +23,7 @@ void Server::parseLocationBlock(std::string &locationBlock) {
         throw std::runtime_error("Location url_type is not validate");
 
 
-    loc.url_type = locationString[1];
+    loc.url_type = locationString.back();
 
     while (true) {
         char cursor = locationBlock[idx];
@@ -47,27 +47,30 @@ bool Server::allocateRoot(std::vector<std::string>& strs, size_t size, Location 
         throw std::runtime_error("root field is only one parameter");
     if (strs[size - 1].back() != ';')
         throw std::runtime_error("Missing semicolon in 'root' directive");
-    strs[1].erase(strs[1].size() - 1);
+    strs.back().erase(strs.back().size() - 1);
     loc.root = strs[1];
     return true;
 }
 
 bool Server::allocateIndex(std::vector<std::string>& strs, size_t size, Location &loc) {
-    if (size != 2)
-        throw std::runtime_error("index field is only one parameter");
+    if (size < 2)
+        throw std::runtime_error("index field have least one parameter");
     if (strs[size - 1].back() != ';')
         throw std::runtime_error("Missing semicolon in 'index' directive");
-    strs[1].erase(strs[1].size() - 1);
-    loc.index = strs[1];
+    strs.back().erase(strs.back().size() - 1);
+    for (size_t i = 1; i < size; ++i)
+        loc.index.push_back(strs[i]);
     return true;
 }
 
 bool Server::allocateMethods(std::vector<std::string>& strs, size_t size, Location &loc) {
+    // 할당 전에 loc.methods를 0으로 초기화하는 이유는, 두 줄에 걸쳐서 methods를 설정할 경우 이전 값이 무시되기 때문
+    loc.methods = 0;
     if (size > 4)
         throw std::runtime_error("methods field has more than 4 parameters");
     if (strs[size - 1].back() != ';')
         throw std::runtime_error("Missing semicolon in 'methods' directive");
-    strs[size - 1].erase(strs[size - 1].size() - 1);
+    strs.back().erase(strs.back().size() - 1);
     for (size_t i = 1; i < size; ++i) {
         if (strs[i] == "GET")
             loc.methods |= 1 << 0;
@@ -86,8 +89,8 @@ bool Server::allocateAutoIndex(std::vector<std::string>& strs, size_t size, Loca
         throw std::runtime_error("Missing semicolon in 'auto_index' directive");
     if (size != 2)
         throw std::runtime_error("auto_index must have only one number");
-    strs[1].erase(strs[1].size() - 1);
-    if (strs[1] == "on")
+    strs.back().erase(strs.back().size() - 1);
+    if (strs.back() == "on")
         loc.auto_index = true;
     else
         loc.auto_index = false;
@@ -117,24 +120,18 @@ bool Server::allocateRedirect(std::vector<std::string>& strs, size_t size, Locat
 }
 
 bool Server::settingLocationBlock(std::vector<std::string>& strs, size_t size, Location &loc) {
-    // 중복은 무시
-    static bool _return = false;
-    static bool root = false;
-    static bool index = false;
-    static bool methods = false;
-    static bool autoindex = false;
-
-    if (!_return && strs[0] == "return")
-        _return = allocateRedirect(strs, size, loc);
-    else if (!root && strs[0] == "root")
-        root = allocateRoot(strs, size, loc);
-    else if (!index && strs[0] == "index")
-        index = allocateIndex(strs, size, loc);
-    else if (!methods && strs[0] == "methods")
-        methods = allocateMethods(strs, size, loc);
-    else if (!autoindex && strs[0] == "autoindex")
-        autoindex = allocateAutoIndex(strs, size, loc);
-    else
-        return false;
-    return true;
+    if (return_flag && strs[0] == "return")
+        return true;
+    // return은 중복 시, 첫 번째 것만 적용 나머지 키워드는 중복 시, 마지막 것만 적용
+    if (!return_flag && strs[0] == "return")
+        return return_flag = allocateRedirect(strs, size, loc);
+    if (strs[0] == "root")
+        return allocateRoot(strs, size, loc);
+    if (strs[0] == "index")
+        return allocateIndex(strs, size, loc);
+    if (strs[0] == "methods")
+        return allocateMethods(strs, size, loc);
+    if (strs[0] == "autoindex")
+        return allocateAutoIndex(strs, size, loc);
+    return false;
 }
